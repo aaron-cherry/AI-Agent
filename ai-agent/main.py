11d1e7ee29c187ba53acd14cd3c5b6aef39ce573
aaron-cherry <aaron.cherry10@gmail.com>
2026-01-11 17:48:07 -0800

    prep for multi-input conversations

--- ai-agent/main.py
+++ ai-agent/main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,15 +11,22 @@ if api_key == None:
 
 parser = argparse.ArgumentParser(description="Prompts Google Gemini with arguement and displays response")
 parser.add_argument("prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
 
 client = genai.Client(api_key=api_key)
 
-content = client.models.generate_content(model="gemini-2.5-flash", contents=args.prompt)
+response_content = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 
-print(f"""
-Prompt tokens: {content.usage_metadata.prompt_token_count}
-Response tokens: {content.usage_metadata.candidates_token_count}
-      """)
+verbose_debug = f"""
+User prompt: {args.prompt}
+Prompt tokens: {response_content.usage_metadata.prompt_token_count}
+Response tokens: {response_content.usage_metadata.candidates_token_count}
+      """
 
-print(content.text)
+if args.verbose:
+    print(verbose_debug)
+
+print(response_content.text)
